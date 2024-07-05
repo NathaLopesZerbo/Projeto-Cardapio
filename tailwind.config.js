f0e3ccae058f71d641b117748179da39b3c37d1b
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2024-07-05 15:47:49 -0300

    teste

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
-    backgroundImage:{
-      "home": "url('/src/assets/bg.png')"
-    }
+    
   },
   plugins: [],
 }
